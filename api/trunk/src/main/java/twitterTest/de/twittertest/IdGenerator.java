8e5d9f2ca5cbe34744c41a97eb183e3319f110ab
Tobias <Tobias@0ae2203c-c003-4152-9762-43d73581c209>
2015-03-04 12:02:13 +0000

    work as designeoinfoweiön

--- api/trunk/src/main/java/twitterTest/de/twittertest/IdGenerator.java
+++ api/trunk/src/main/java/twitterTest/de/twittertest/IdGenerator.java
@@ -4,6 +4,7 @@ public class IdGenerator {
 	private Integer id = Integer.MIN_VALUE;
 
 	public synchronized int getId() {
-		return ++id;
+		id++;
+		return id;
 	}
 }
--- api/trunk/src/main/java/twitterTest/de/twittertest/TwitterTest.java
+++ api/trunk/src/main/java/twitterTest/de/twittertest/TwitterTest.java
@@ -4,21 +4,8 @@ import java.io.IOException;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.IntField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.BooleanClause.Occur;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.NumericRangeQuery;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
@@ -38,17 +25,9 @@ public class TwitterTest {
 		// To store an index on disk, use this instead:
 		// Directory directory = FSDirectory.open("/tmp/testindex");
 		IndexWriterConfig config = new IndexWriterConfig(analyzer);
-		final IndexWriter iwriter = new IndexWriter(directory, config);
-		Document doc = new Document();
-		Integer id = idGenerator.getId();
-		doc.add(new IntField("id", id, IntField.TYPE_STORED));
-		doc.add(new Field("text", "Frage", TextField.TYPE_NOT_STORED));
-		iwriter.addDocument(doc);
-		iwriter.commit();
-		DirectoryReader ireader = DirectoryReader.open(directory);
-		IndexSearcher isearcher = new IndexSearcher(ireader);
+		IndexWriter iwriter = new IndexWriter(directory, config);
 		UserStreamListener listener = new MyUserStreamListener(idGenerator,
-				iwriter, isearcher, analyzer, tweetHolder);
+				directory, analyzer, tweetHolder, iwriter);
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 		twitterStream.addListener(listener);
 		twitterStream.user();
@@ -68,28 +47,28 @@ public class TwitterTest {
 		//
 		// Now search the index:
 
-		if (DirectoryReader.indexExists(directory)) {
-			QueryParser parser = new QueryParser("text", analyzer);
-			// Parse a simple query that searches for "text":
-			Query text = null;
-			try {
-				text = parser.parse("text:Frage");
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			BooleanQuery query = new BooleanQuery();
-			Query idQuery = NumericRangeQuery.newIntRange("id", id, id, true,
-					true);
-			query.add(idQuery, Occur.MUST);
-			query.add(text, Occur.MUST);
-			ScoreDoc[] hits = isearcher.search(query, 1000).scoreDocs;
-			// Iterate through the results:
-			for (int i = 0; i < hits.length; i++) {
-				Document hitDoc = isearcher.doc(hits[i].doc);
-				System.out.println(hitDoc.get("id"));
-			}
-			ireader.close();
-			directory.close();
-		}
+		// if (DirectoryReader.indexExists(directory)) {
+		// QueryParser parser = new QueryParser("text", analyzer);
+		// // Parse a simple query that searches for "text":
+		// Query text = null;
+		// try {
+		// text = parser.parse("text:Frage");
+		// } catch (ParseException e) {
+		// e.printStackTrace();
+		// }
+		// BooleanQuery query = new BooleanQuery();
+		// Query idQuery = NumericRangeQuery.newIntRange("id", id, id, true,
+		// true);
+		// query.add(idQuery, Occur.MUST);
+		// query.add(text, Occur.MUST);
+		// ScoreDoc[] hits = isearcher.search(query, 1000).scoreDocs;
+		// // Iterate through the results:
+		// for (int i = 0; i < hits.length; i++) {
+		// Document hitDoc = isearcher.doc(hits[i].doc);
+		// System.out.println(hitDoc.get("id"));
+		// }
+		// ireader.close();
+		// directory.close();
+		// }
 	}
 }

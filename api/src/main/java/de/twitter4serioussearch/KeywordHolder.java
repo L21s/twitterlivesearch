aba332bba9a3cb66e95c07b7def5dc4e4c546100
Tobias <Tobias@0ae2203c-c003-4152-9762-43d73581c209>
2015-03-18 14:33:33 +0000

    JavaDoc

--- api/src/main/java/de/twitter4serioussearch/KeywordHolder.java
+++ api/src/main/java/de/twitter4serioussearch/KeywordHolder.java
@@ -6,47 +6,78 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import de.twitter4serioussearch.common.Util;
+
 public class KeywordHolder {
-	// mapping von keyword zu map<sessionId, Listener>
+	// mapping von query zu map<sessionId, Listener>
 	private Map<String, Map<String, TweetListener>> keywords = Collections
 			.synchronizedMap(new HashMap<>());
 	// mapping von sessionId zu List<keyword>
-	private Map<String, List<String>> keywordsForSessions = Collections
+	private Map<String, List<String>> queriesForSessions = Collections
 			.synchronizedMap(new HashMap<>());
 
 	public Map<String, Map<String, TweetListener>> getKeywords() {
 		return keywords;
 	}
 
-	public void putKeyword(String keyword, String sessionId,
+	/**
+	 * Registriert einen {@link de.twitter4serioussearch.TweetListener
+	 * TweetListener} für die Kombination aus Query und Session
+	 *
+	 * @param query
+	 *            Vom User gesuchter String
+	 * @param sessionId
+	 *            eindeutiger Session Identifier (Hintergrund: Die gleiche Query
+	 *            kann von mehreren Usern registriert werden)
+	 * @param actionListener
+	 *            {@link de.twitter4serioussearch.TweetListener TweetListener}
+	 *            der invoked wird, sobald ein zum query passender Tweet
+	 *            empfangen wurde
+	 */
+	public void registerQuery(String query, String sessionId,
 			TweetListener actionListener) {
-		Map<String, TweetListener> listeners = keywords.get(keyword);
+		Map<String, TweetListener> listeners = keywords.get(query);
 		if (listeners == null) {
 			listeners = Collections
 					.synchronizedMap(new HashMap<String, TweetListener>());
 			listeners.put(sessionId, actionListener);
-			keywords.put(keyword, listeners);
+			keywords.put(query, listeners);
 			return;
 		}
 		listeners.put(sessionId, actionListener);
-		List<String> keywordsForSession = keywordsForSessions.get(sessionId);
+		List<String> keywordsForSession = queriesForSessions.get(sessionId);
 		if (keywordsForSession == null) {
 			keywordsForSession = new ArrayList<>();
-			keywordsForSession.add(keyword);
-			keywordsForSessions.put(sessionId, keywordsForSession);
+			keywordsForSession.add(query);
+			queriesForSessions.put(sessionId, keywordsForSession);
 		}
+		keywordsForSession.add(query);
 	}
 
-	public void removeKeyword(String keyword, String sessionId) {
-		Map<String, TweetListener> listeners = keywords.get(keyword);
-		if (listeners == null) {
-			return;
-		}
-		listeners.remove(sessionId);
+	/**
+	 * Deregistriert ein Query für die gegebene Session
+	 *
+	 * @param query
+	 *            zu unregistrierendes Query
+	 * @param sessionId
+	 *            eindeutiger Session Identifier (Hintergrund: Die gleiche Query
+	 *            kann von mehreren Usern registriert werden)
+	 */
+	public void unregisterQuery(String query, String sessionId) {
+		Map<String, TweetListener> listeners = keywords.get(query);
+		Util.safe(listeners).remove(sessionId);
 	}
 
-	public void removeSession(String sessionId) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * Deregistriert alle querys für eine gegebene session
+	 *
+	 * @param sessionId
+	 *            eindeutiger Session Identifier
+	 */
+	public void unregisterSession(String sessionId) {
+		for (String query : Util.safe(queriesForSessions.get(sessionId))) {
+			unregisterQuery(query, sessionId);
+		}
+		queriesForSessions.remove(sessionId);
 	}
 }

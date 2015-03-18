beb4f6faaeff8c5317c0ac23dc9c29d0f6b80cdb
Tobias <Tobias@0ae2203c-c003-4152-9762-43d73581c209>
2015-03-18 15:07:08 +0000

    idgenerator

--- trunk/api/src/main/java/de/twitter4serioussearch/IdGenerator.java
+++ trunk/api/src/main/java/de/twitter4serioussearch/IdGenerator.java
@@ -2,9 +2,18 @@ package de.twitter4serioussearch;
 
 public class IdGenerator {
 	private Integer id = Integer.MIN_VALUE;
+	private Integer MAX_NUMBER_OF_TWEETS;
 
-	public synchronized int getId() {
-		id++;
-		return id;
+	public IdGenerator(Integer maxNumberOfTweets) {
+		MAX_NUMBER_OF_TWEETS = maxNumberOfTweets;
+	}
+
+	public synchronized int getNextId() {
+		id = (id + 1) % MAX_NUMBER_OF_TWEETS;
+		return id - 1;
+	}
+
+	public synchronized int getIdToRemove() {
+		return (id + 1) % MAX_NUMBER_OF_TWEETS;
 	}
 }

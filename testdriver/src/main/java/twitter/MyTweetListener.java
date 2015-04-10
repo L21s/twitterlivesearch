b96e4372ca57ffd27bbd729d378992a762eefc29
Tobias <Tobias@0ae2203c-c003-4152-9762-43d73581c209>
2015-04-10 12:07:57 +0000

    Änderungen für Demo

--- testdriver/src/main/java/twitter/MyTweetListener.java
+++ testdriver/src/main/java/twitter/MyTweetListener.java
@@ -4,21 +4,29 @@ import java.io.IOException;
 
 import javax.websocket.Session;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import twitter4j.Status;
 import de.twitter4serioussearch.api.TweetListener;
 
 public class MyTweetListener implements TweetListener {
 
 	private Session session;
-	
+	private Logger log = LogManager.getLogger();
+
 	public MyTweetListener(Session session) {
 		this.session = session;
 	}
-	
+
 	@Override
 	public void handleNewTweet(Status tweet) {
 		try {
-			session.getBasicRemote().sendText(TwitterMaschine.createTweetMessage(tweet).toString());
+			session.getBasicRemote().sendText(
+					TwitterMaschine.createTweetMessage(tweet).toString());
+			log.trace("sent text "
+					+ TwitterMaschine.createTweetMessage(tweet).toString()
+					+ " to session " + session);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
